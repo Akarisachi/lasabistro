98b37a3a780cdf8ca1150dd4ecf44bb024d5a066
Akarisachi <144439240+Akarisachi@users.noreply.github.com>
2025-12-17 14:27:42 +0800

    Update customer_app.py

--- customer_app.py
+++ customer_app.py
@@ -408,7 +408,9 @@ def send_message():
             "message": "Failed to send email"
         }), 500
 
-if __name__ == "__main__":
-    # Run on port 5001 for customer backend
-    app.run(host="0.0.0.0", port=5001, debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
